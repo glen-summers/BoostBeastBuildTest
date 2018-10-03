42fcc47735546d35e66cdbbf02bf88a7eb0c8210
Glen Summers <glen.summers@outlook.com>
2018-10-03 18:03:21 +0100

    detect gcc ver use params for app dir gcc jamroot

--- go.sh
+++ go.sh
@@ -12,6 +12,15 @@ SSL_VER=openssl-1.1.0i
 SSL_URL=https://www.openssl.org/source
 
 WGET_OPT="--secure-protocol=auto --no-check-certificate"
+
+VARIANT=release
+LINK=static
+THREADING=multi
+RUNTIME_LINK=static
+ADDRESS_MODEL=64
+ARCHITECTURE=x86
+LAYOUT=versioned
+MODULES=system,date_time
 ###########################################################################
 
 error_exit() {
@@ -74,27 +83,20 @@ BuildBoost() {
 		./bootstrap.sh || error_exit "bootstrap failed"
 	fi
 
-	#local VARIANT=debug,release
-	local VARIANT=release
-	local LINK=static
-	local THREADING=multi
-	local RUNTIME_LINK=static
-	local ADDRESS_MODEL=64
-	local ARCHITECTURE=x86
-	local MODULES=system,date_time,test
-	
-	# causes debug,release build error, tagged causes link error
-	local LAYOUT=versioned
-
 	local WITH_MODULES=""
 	if [[ ! -z "$MODULES" ]]; then 
 		WITH_MODULES="--with-${MODULES//,/ --with-}"
 	fi
 
-	local B2_OPTS="variant=$VARIANT link=$LINK threading=$THREADING runtime-link=$RUNTIME_LINK address-model=$ADDRESS_MODEL architecture=$ARCHITECTURE $WITH_MODULES --layout=$LAYOUT"
-
 	echo ./b2 $B2_OPTS
-	./b2 $B2_OPTS || error_exit "B2 Boost build failed"
+	./b2 $B2_OPTS $WITH_MODULES || error_exit "B2 Boost build failed"
+	
+	#boost build seems to try and link without the x64\x32 in the lib file name
+	#    boost_system-gcc73-mt-s-1_68
+	# libboost_system-gcc73-mt-s-x64-1_68
+	GCC_LIB_VER="gcc$GCC_MAJ$GCC_MIN"
+	cp stage/lib/libboost_system-$GCC_LIB_VER-mt-s-x$ADDRESS_MODEL-$BOOST_MAJ"_"$BOOST_MIN.a stage/lib/libboost_system-$GCC_LIB_VER-mt-s-$BOOST_MAJ"_"$BOOST_MIN.a || error_exit "lib copy failed"
+
 	popd
 }
 
@@ -105,8 +107,10 @@ BuildSsl() {
 	if [[ -f "$SSL_TARGET/include/openssl/opensslconf.h" ]]; then
 		echo "openssl present"
 	else
-		GetTarGz "$SSL_URL" "$SSL_ARCHIVE" "$SSL_TARGET"
-
+		if [[ ! -d "$TEMP_DIR/$SSL_VER" ]]; then
+			GetTarGz "$SSL_URL" "$SSL_ARCHIVE" "$TEMP_DIR"
+		fi
+		
 		echo "building $SSL_TARGET"
 		pushd "$TEMP_DIR/$SSL_VER"
 		# chmod +x ./Configure || error_exit "chmod failed"
@@ -115,40 +119,49 @@ BuildSsl() {
 		chmod +x ./config || error_exit "chmod failed"
 		./config --prefix="$SSL_TARGET" --openssldir="$SSL_TARGET/ssl" || error_exit "config failed"
 		
-		echo make || error_exit "make failed"
-		echo make install || error_exit "make install failed"
+		make || error_exit "make failed"
+		make install || error_exit "make install failed"
 		popd
 
+		#clean intermediate ssl...
 		#rm -r "$TEMP_DIR/$SSL_VER" || error_exit "rm ssl temp dir failed"
 	fi
 }
 
 BuildTestApp() {
-	pushd "$ROOT"
+	pushd "$ROOT/gcc"
 	export BOOST_LIBRARY_PATH="$BOOST_TARGET"
 	export SSL_LIBRARY_PATH="$SSL_TARGET"
 	export BOOST_MAJ="$BOOST_MAJ"
 	export BOOST_MIN="$BOOST_MIN"
 
-	$BOOST_TARGET/b2 Jamroot -sBOOST_ROOT=$BOOST_TARGET -d2 --layout=versioned || error_exit "Build failed"
-
-	#parms
-	#./bin/gcc-7.3.0/release/address-model-64/architecture-x86/link-static/runtime-link-static/threading-multi/App1
+	$BOOST_TARGET/b2 $B2_OPTS -sBOOST_ROOT=$BOOST_TARGET -d2 toolset=gcc || error_exit "Build failed"
+	$APP_BUILD_DIR/App1 || error_exit "App1 failed"
 	popd
 }
 
 Init() {
 	ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 	TEMP_DIR="$ROOT/tempFiles"
-	BUILD="$ROOT/bin"
+	BUILD="$ROOT/gcc/bin"
 
-	ABOVE="$( FindDirectoryAbove $ROOT $DEP_DIR )"
+	local ABOVE="$( FindDirectoryAbove $ROOT $DEP_DIR )"
 	if [[ -z "$ABOVE" ]]; then
 		TARGET_DIR="$TEMP_DIR"
 	else
 		TARGET_DIR="$ABOVE/$DEP_DIR"
 	fi
 	echo "TARGET_DIR = $TARGET_DIR"
+
+	B2_OPTS="variant=$VARIANT link=$LINK threading=$THREADING runtime-link=$RUNTIME_LINK address-model=$ADDRESS_MODEL architecture=$ARCHITECTURE --layout=$LAYOUT"
+
+	local GCC_VER="$(gcc --version | grep ^gcc | sed 's/^.* //g')"
+	local REGEX="([0-9]+)\.([0-9]+)\.[0-9]"
+	[[ $GCC_VER =~ $REGEX ]] || error_exit "failed to parse gcc version"
+	GCC_MAJ="${BASH_REMATCH[1]}"
+	GCC_MIN="${BASH_REMATCH[2]}"
+
+	APP_BUILD_DIR="$BUILD/gcc-$GCC_VER/$VARIANT/address-model-$ADDRESS_MODEL/architecture-$ARCHITECTURE/link-$LINK/runtime-link-$RUNTIME_LINK/threading-$THREADING"
 }
 
 Build() {
@@ -158,10 +171,14 @@ Build() {
 }
 
 Clean() {
-	rm -r "$TEMP_DIR" || error_exit "rm temp dir failed"
+	rm -f -r "$TEMP_DIR" || error_exit "rm temp dir failed"
+	rm -f -r "$BUILD" || error_exit "rm build dir failed"
 }
 
+###########################################################################
+
 clear
+
 echo "go $1..."
 case "$1" in
 	"")
